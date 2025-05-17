1bde2ad2e29456e89d4388c5d516d8a60afb6521
Adanbiofisico <109325626+Adanbiofisico@users.noreply.github.com>
2025-05-16 21:11:26 -0600

    app.py

--- app.py
+++ app.py
@@ -318,7 +318,4 @@ def obtener_estado_fatiga(atleta_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    # Añade este print para verificar en logs
-    print(f"Starting server on port {port}")
-    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False en producción
+    app.run(debug=True, host='0.0.0.0', port=5000)
