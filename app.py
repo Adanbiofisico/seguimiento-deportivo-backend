56babcec902c829cc6658947e0719f32a35baa28
Adanbiofisico <109325626+Adanbiofisico@users.noreply.github.com>
2025-05-19 16:15:41 -0600

    app.py

--- app.py
+++ app.py
@@ -317,5 +317,7 @@ def obtener_estado_fatiga(atleta_id):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
