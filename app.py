9bff697a214f400e30d15b375d6afecd82ec936e
Adanbiofisico <109325626+Adanbiofisico@users.noreply.github.com>
2026-02-26 19:29:03 -0600

    app.py

--- app.py
+++ app.py
@@ -524,5 +524,4 @@ def get_hrv(id_atleta):
 
 # ——— Arranque compatible con Render ———
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
